8b402e1b93741fb2e935d2c46fedd9e4b587b086
qianfengg <fred_gqf0221@163.com>
2022-11-15 09:41:15 +0800

    stop及onStop

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,7 +1,12 @@
+import { extend } from "../../shared";
+
 const targetMap = new Map();
 let activeEffect;
 class ReactiveEffect {
   private _fn: any;
+  deps = [];
+  active = true;
+  onStop?: () => void;
   constructor(fn, public scheduler?) {
     this._fn = fn;
   }
@@ -9,28 +14,48 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 export const effect = (fn, options: any = {}) => {
   const scheduler = options.scheduler;
   const _effect = new ReactiveEffect(fn, scheduler);
-  _effect.run();
-  return _effect.run.bind(_effect);
+  extend(_effect, options);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  runner();
+  return runner;
 };
 
 export const track = (target, key) => {
   // targetMap => target => depsMap
-  // depsMap => key => deps
+  // depsMap => key => dep
   let depsMap = targetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
     targetMap.set(target, depsMap);
   }
-  let deps = depsMap.get(key);
-  if (!deps) {
-    deps = new Set();
-    depsMap.set(key, deps);
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
   }
-  deps.add(activeEffect);
+  if (!activeEffect) return;
+  dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 };
 export const trigger = (target, key) => {
   const depsMap = targetMap.get(target);
@@ -42,4 +67,8 @@ export const trigger = (target, key) => {
       effect.run();
     }
   }
+};
+
+export const stop = (runner) => {
+  runner.effect.stop();
 };
